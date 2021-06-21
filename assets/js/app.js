7018b118ee4e91dcb2fdad40df42325db0270da5
emirsagit <37173514+emirsagit@users.noreply.github.com>
2021-06-21 20:31:03 +0300

    Add javascript navigation toggle functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,10 @@
-function toggle(e) {
-  e.preventDefault();
-  console.log("burada");
+var navigation = document.getElementById("navigation");
+
+
+function toggle() {
+  navigation.classList.toggle('mobile-hidden');
 }
 
+document.getElementById("close-button").addEventListener("click", toggle);
 document.getElementById("hamburger").addEventListener("click", toggle);
+
