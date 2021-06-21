b08b65c19ce82db84902318aeadafe6267c76a1e
emirsagit <37173514+emirsagit@users.noreply.github.com>
2021-06-21 22:36:27 +0300

    Fix linters errors

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,8 @@
-var navigation = document.getElementById("navigation");
-
+const navigation = document.getElementById('navigation');
 
 function toggle() {
   navigation.classList.toggle('mobile-hidden');
 }
 
-document.getElementById("close-button").addEventListener("click", toggle);
-document.getElementById("hamburger").addEventListener("click", toggle);
-
+document.getElementById('close-button').addEventListener('click', toggle);
+document.getElementById('hamburger').addEventListener('click', toggle);
